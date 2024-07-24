20616800b5f8d366cd3780d9b11ce52b398433ea
fred921110 <vito86077@gmail.com>
2024-07-24 10:26:51 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CodeEditor from './Coder';
+
+function App() {
+
+  return (
+    <>
+      <h1>Ace Editor in React</h1>
+      <CodeEditor />
+    </>
+  )
+}
+
+export default App
