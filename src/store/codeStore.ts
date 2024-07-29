8676b3e09b069385c4c5f5bbfff6d6c8cbe9b3e5
fred921110 <vito86077@gmail.com>
2024-07-29 15:01:39 +0800

    feat: id gen by hash

--- src/store/codeStore.ts
+++ src/store/codeStore.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand"
+
 import { Position } from "../wasm_exec"
 import { persist, createJSONStorage } from 'zustand/middleware'
+import { genId } from "../utils/id";
 
 
 export type CodeStore = {
@@ -66,21 +68,30 @@ export const setSelectList = (str: string) => {
     }))
 }
 // setting select code
-export const setSetted = (context: string, name = "", pos: Position) => {
+export const setSetted = (context: string, name = "", pos: Position): string|number => {
     const list = useCodeStore.getState().setterList
     const exeists = list?.find((item) => item.content === context)
     if (exeists) {
+        const updatedList = list?.map((item) => {
+            if (item.content === context) {
+                return { ...item, name, pos }
+            }
+            return item
+        })
         useCodeStore.setState(() => ({
-            setterList: list,
+            setterList: updatedList,
         }))
+        return exeists.id
     }
+    const id =genId(context)
     if (!list) {
         useCodeStore.setState(() => ({
-            setterList: [{ id: Date.now(), content: context, name, pos }],
+            setterList: [{ id, content: context, name, pos }],
         }))
     } else {
         useCodeStore.setState(() => ({
-            setterList: [...list, { id: Date.now(), content: context, name, pos }],
+            setterList: [...list, { id, content: context, name, pos }],
         }))
     }
+    return id
 }
