29dbcc36058a4fb70ecd1574f4757b2fb59dcc49
fred921110 <vito86077@gmail.com>
2024-07-29 12:17:37 +0800

    移除不必要的内容

--- src/wasm_exec.d.ts
+++ src/wasm_exec.d.ts
@@ -6,4 +6,20 @@ declare class Go {
 
 interface GoWasm {
     add(a: number, b: number): number;
+    parser(a: string): string;
+}
+
+export interface ParserReq {
+    content: string;
+    pos?: Position;
+}
+export interface Position {
+    start: number;
+    end: number;
+}
+
+export interface Response<T> {
+    code: number;
+    msg: string;
+    data: T;
 }
